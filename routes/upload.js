3cd38834ed594e510754bf920acd91093ae5aeaa
darkdevil0012 <Smarthamza123@outlook.com>
2026-01-15 00:24:52 +0500

    Updated correct code

--- routes/upload.js
+++ routes/upload.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 
 const uploadRouter = express.Router();
+
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		cb(null, "uploads/");
